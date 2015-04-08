0a7100a3f7e3cbd28292e64f5a58fe983616b265
Malinda Mosholder <m.m.mosholder@gmail.com>
2015-04-08 15:27:17 -0600

    worked on bonus 1

--- main.js
+++ main.js
@@ -29,87 +29,84 @@
 
 // alert(victimsList[0] + volList[0] + victimsList[1] + volList[1])
 
-// var victimAmount = prompt("How many victims?");
+var victimAmount = prompt("How many victims?");
+var victims = [];
+var victimNames = [];
+var victimStreets = [];
+
+for(var i = 0; i < victimAmount.length; i++) {
+	var victim = {
+		vicName:  prompt("Victim Name?"),
+		vicStreet: prompt("Victim Street?")
+	};
+
+	victims.push(victim);
+}
+for(var i = 0; i < victimAmount; i++) {
+	victimNames.push(victims[i].vicName);
+}
+for(var i = 0; i < victimAmount; i++) {
+	victimStreets.push(victims[i].vicStreet);
+}
+
+var volAmount = prompt("How many volunteers?");
+var volunteers = [];
+var volNames = [];
+var volStreets = [];
+
+for(var i = 0; i < volAmount; i++) {
+	var volunteer = {
+		volName: prompt("Volunteer Name?"),
+		volStreet: prompt("Volunteer Street?")
+	};
+
+	volunteers.push(volunteer);
+}
+for(var i = 0; i < volAmount; i++) {
+	volNames.push(volunteers[i].volName);
+}
+for(var i = 0; i < volAmount; i++) {
+	volStreets.push(volunteers[i].volStreet);
+}
+
+var inNeed = prompt("Enter the name of a person in need");
+	for(var i = 0; i < victimAmount; i++) {
+		if(inNeed === victims[i].vicName) {
+			var streetMatch = victims[i].vicStreet;
+
+			for(var j = 0; j < volunteers.length; j++) {
+				if(volunteers[j].volStreet === streetMatch) {
+					console.log(volunteers[j], victims[i]);
+				} 
+			}
+		}
+	}
+
+
+
+// alert("There are " + victimAmount + " people in need" + "." + "There are " + volAmount + " volunteers" + ".\n"
+// 	+ "The victim names are " + victimNames + ",\n" + "The volunteer names are " + volNames)
+
+
+
+
+
+// ConfirmExercise
 // var victims = [];
 // var victimNames = [];
 
-// for(var i = 0; i <= victimAmount.length; i++) {
+// var r = true;
+
+// while (r) {
 // 	var victim = {
 // 		vicName:  prompt("Victim Name?"),
 // 		vicNumber: prompt("Victim Number?")
 // 	};
 
 // 	victims.push(victim);
-// };
-// for(i = 0; i <= victimAmount.length; i++) {
-// 	victimNames.push(victims[i].vicName);
-// };
 
-// var volAmount = prompt("How many volunteers?");
-// var volunteers = [];
-// var volNames = [];
+// 	r = confirm("More victims to enter?");
 
-// for(var i = 0; i <= volAmount.length; i++) {
-// 	var volunteer = {
-// 		volName: prompt("Volunteer Name?"),
-// 		volNumber: prompt("Volunteer number?")
-// 	};
-
-// 	volunteers.push(volunteer);
-// };
-// for(i = 0; i <= volAmount.length; i++) {
-// 	volNames.push(volunteers[i].volName);
 // };
 
-// alert("There are " + victimAmount + " people in need" + "." + "There are " + volAmount + " volunteers" + ".\n"
-// 	+ "The victim names are " + victimNames + ",\n" + "The volunteer names are " + volNames)
 
-var victims = [];
-var victimNames = [];
-
-var r = true;
-
-while (r) {
-	var victim = {
-		vicName:  prompt("Victim Name?"),
-		vicNumber: prompt("Victim Number?")
-	};
-
-	victims.push(victim);
-
-	r = confirm("More victims to enter?");
-
-};
-
-var volunteers = [];
-var volNames = [];
-
-while (r) {
-	var volunteer = {
-		volName: prompt("Volunteer Name?"),
-		volNumber: prompt("Volunteer Number?")
-	};
-
-	volunteers.push(volunteer);
-
-	r = confirm("More victims to enter?");
-}
-
-// var volAmount = prompt("How many volunteers?");
-// var volunteers = [];
-// var volNames = [];
-
-// for(var i = 0; i <= volAmount.length; i++) {
-// 	var volunteer = {
-// 		volName: prompt("Volunteer Name?"),
-// 		volNumber: prompt("Volunteer number?")
-// 	};
-
-// 	volunteers.push(volunteer);
-// };
-// for(i = 0; i <= volAmount.length; i++) {
-// 	volNames.push(volunteers[i].volName);
-// };
-
-// alert("There are " + victimAmount + " people in need" + "." + "There are " + volAmount + " volunteers" + ".\n"
-// 	+ "The victim names are " + victimNames + ",\n" + "The volunteer names are " + volNames)
